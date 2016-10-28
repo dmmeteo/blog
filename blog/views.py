9e7e94cf0886b6be682efcf5ad5f4d9a4c3227bd
Dmitry Sichkar <dmmeteo@gmail.com>
2016-10-28 18:58:20 +0300

    Posts & Post views

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
 # Create view list of posts
 def post_list(request):
+	# Get post list which published_date not empty
+	# and order by published_date
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	return render(request, 'post_list.html', {'posts': posts})
 
+def post_detail(request, pk):
+	# Get post by primary_key(pk) or 404
+	post = get_object_or_404(Post, pk=pk)
+	return render(request, 'post_detail.html', {'post': post})
--- mysite/urls.py
+++ mysite/urls.py
@@ -2,9 +2,8 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'mysite.views.home', name='home'),
-    url(r'^$', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+	# blog app urls
+	url(r'', include('blog.urls')),
+	# admin urls
+	url(r'^admin/', include(admin.site.urls)),
 ]
