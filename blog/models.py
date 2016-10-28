ae258ac4cdb1b2b1c88b1aca13bf6c6f559c0835
Dmitry Sichkar <dmmeteo@gmail.com>
2016-10-28 14:41:54 +0300

    Model & Admin

---
+++ blog/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.utils import timezone
+
+# Create models Post
+class Post(models.Model):
+
+	# create fields
+	author = models.ForeignKey('auth.User')
+	title = models.CharField(max_length=200)
+	text = models.TextField()
+	created_date = models.DateTimeField(default=timezone.now)
+	published_date = models.DateTimeField(blank=True, null=True)
+
+	# publish date function
+	def publish(self):
+		self.published_date = timezone.now()
+		self.save()
+
+	# return string title
+	def __str__(self):
+		return self.title
